fa913cc3626af363f85c05921d28b5b0196b1b7e
Henry Maher <henry.maher99@gmail.com>
2019-09-24 11:53:54 +0100

    add image and content to title page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import React from 'react';
 import './App.css';
+import title_page from './images/title.JPG';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Image from 'react-bootstrap/Image';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      </header>
+      <Container fluid id="title">
+        <Row>
+          <Col>
+            <Image src={title_page}></Image>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <h1> ROBERT GEORGE SANDER </h1>
+            <h4> Designer/Artist/Pornstar/Basketballer </h4>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
