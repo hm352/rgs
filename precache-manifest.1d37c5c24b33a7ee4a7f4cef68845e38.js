cec9647d4adf96be73eecc4937c0b650e524fe27
Henry Maher <henry.maher99@gmail.com>
2019-09-24 14:16:20 +0100

    Updates

---
+++ precache-manifest.1d37c5c24b33a7ee4a7f4cef68845e38.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a1514255127f98c2d7568b89396ae45",
+    "url": "/rgs/index.html"
+  },
+  {
+    "revision": "d2723a488ef925b030d3",
+    "url": "/rgs/static/css/main.0c456df7.chunk.css"
+  },
+  {
+    "revision": "4f2ca12075edfaa7d6db",
+    "url": "/rgs/static/js/2.08131770.chunk.js"
+  },
+  {
+    "revision": "d2723a488ef925b030d3",
+    "url": "/rgs/static/js/main.ec864360.chunk.js"
+  },
+  {
+    "revision": "a7fb9f74bfb161706bf2",
+    "url": "/rgs/static/js/runtime-main.8055d0bb.js"
+  },
+  {
+    "revision": "58504cfdbb23357fc573b3b7ac1f1080",
+    "url": "/rgs/static/media/biomimickry.58504cfd.jpg"
+  },
+  {
+    "revision": "5930773ee0af2172ff50f8340da5d3b9",
+    "url": "/rgs/static/media/lad.5930773e.jpg"
+  },
+  {
+    "revision": "60ab4a0353de83b4fb3a2b08b1f122c0",
+    "url": "/rgs/static/media/paintings.60ab4a03.JPG"
+  },
+  {
+    "revision": "59712e98644129ab57270ab9830ea8f9",
+    "url": "/rgs/static/media/petrolStation.59712e98.jpg"
+  },
+  {
+    "revision": "5edd8b4dcecf0cd175e80b327289bd2f",
+    "url": "/rgs/static/media/tape.5edd8b4d.JPG"
+  },
+  {
+    "revision": "e4d6e57aecbd043346c4d1e6c9ac37be",
+    "url": "/rgs/static/media/title.e4d6e57a.JPG"
+  }
+]);
